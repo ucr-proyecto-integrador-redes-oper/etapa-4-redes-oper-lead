b01211850efdbebacb04414e6e64b738c6ed2c9f
diegotrejos <48733767+diegotrejos@users.noreply.github.com>
2019-06-27 20:09:04 -0600

    Add files via upload

--- NodoAzul.py
+++ NodoAzul.py
@@ -1,19 +1,77 @@
 #!/usr/bin/env python
 import socket
-from n_nPaq import n_nPaq
-UDP_IP = "127.0.0.1"
-UDP_PORT=8888
+import sys
+import threading
+import struct
+from a_aPaq import a_aPaq
 
-
-
-
-
-#Protocolo Azul Naranja
-test = n_nPaq(2,145,3,0,'r',350,'01.02.03.04',5050,1000)#mete de un solo en cola de entrada
-MESSAGE = test.serialize()
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-sock.sendto(MESSAGE,(UDP_IP,UDP_PORT)) 
-
+try:
+    import queue
+except ImportError:
+    import Queue as queue
 
 
 #Protocolo Azul Azul
+
+#protocolo USL
+
+
+
+class NodoAzul:
+
+    # Aqui se ponen los detalles para ajusta puerto y IP
+    def __init__(self, ip, port, nodeID):
+        self.ip = ip
+        self.port = port
+        self.nodeID = nodeID
+     
+        # self.blueGraphDir = blueGraphDir
+
+    def run(self):
+        server = (self.ip, self.port)
+        colaEntrada = queue.Queue()
+        colaSalida = queue.Queue()
+        package = a_aPaq(0,1,"hola",('10.1.137.29',8887))
+        colaSalida.put(package)
+        # listaVecinos[]
+        # Prepara Hilo que recibe mensajes
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind(server)
+        print("Escuchando: " + self.ip + ":" + str(self.port))
+
+      
+
+        ##Hilos recibidor
+        t = threading.Thread(target=HiloRecibidor, args=(colaEntrada,sock,self.nodeID,colaSalida))
+        t.start()
+        print("hilo recibidor iniciado")
+        # hilo enviador
+        t2 = threading.Thread(target=HiloEnviador, args=(colaSalida, sock))
+        t2.start()
+        print(("hilo enviador iniciado"))
+    
+def HiloRecibidor(colaEntrada,sock,nodeID,colaSalida):
+  while True:
+	  payload, client_address = sock.recvfrom(1024)
+	  print("Paquete obtenido")
+	  package = a_aPaq(0,0,0,client_address)
+	  package.unserialize(payload)
+	  print("Mensaje de Tipo", package.tipo,"De SN=", package.sn,"Que contiene:",package.payload,"Proveniente de:", package.direccion)
+	  colaSalida.put(package)
+           
+      
+
+
+def HiloEnviador(colaSalida, sock):
+
+    while True:
+        ##Takes a package from the queue. If the queue is empty it waits until a package arrives
+      
+       package = colaSalida.get()    
+       print(package.direccion)
+       address = package.direccion
+       pak=package.serialize()
+       sock.sendto(pak, address)
+       print("mensaje enviado")
+
+
