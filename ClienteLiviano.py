73f4e1dd04cd5316af53c98d43d8ab35b0d33569
adavidovich08 <48734553+adavidovich08@users.noreply.github.com>
2019-07-08 01:26:15 -0600

    Add files via upload

--- ClienteLiviano.py
+++ ClienteLiviano.py
@@ -1,5 +1,6 @@
 import socket
 import struct
+import os
 from netifaces import interfaces, ifaddresses, AF_INET
 
 my_ip = ifaddresses(interfaces()[1])[AF_INET].pop(0)['addr']
@@ -9,22 +10,58 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server = (my_ip, my_port)
 sock.bind(server)
 
+#No se puede cambiar el directorio de los archivos desde el primer uso de este cliente
+dirArch = os.getcwd() + "/imagenes"
+directory = os.fsencode(dirArch)
+
+#array para la relacion 1:1 entre ID y nombre de archivo
+arrayArch = ["" for x in range(65535)]  
+
+i = 0
+for file in os.listdir(directory):
+     filename = os.fsdecode(file)
+     arrayArch[i] = filename 
+     i += 1
+     continue
+
 ip_verde = input("IP del Nodo Verde: ")
 port_verde = int(input("Puerto del Nodo Verde: "))
 address = (ip_verde,port_verde)
 
-inputUsuario = input("Que quiere hacer?\n1. Depositar\n2. Existe\n3. Completo\n4. Localizar\n4. Eliminar\n")
+#Escoger la accion
+accion = int(input("Que quiere hacer?\n1. Depositar\n2. Existe\n3. Completo\n4. Obtener\n5. Localizar\n6. Eliminar\n"))
 
 #Escoger el archivo que se quiere consultar
+b = True
+while (b):
+	decision = input("Conoce el ID del archivo? [y/n]")
+	if(decision == 'y'):
+		idArch = int(input("Digite el ID del archivo: "))
+		b = False
+	elif(decision == 'n'):
+		for x in range(i-1):
+			print(x, ": ", arrayArch[x])
+		idArch = int(input("Digite el ID del archivo: "))
+		b = False
 
-#Hay que pensar como queremos hacer nuestros paquetes cliente-verde
-#Probablemente incluir ip y puerto para que nodo verde pueda responder
-paquete = struct.pack('!b', 20) 
+referencia = dirArch + "/" + arrayArch[idArch]
+print(referencia)
+
+#Se manda accion y ID del archivo al nodo verde
+
+if(accion == 1):
+	paquete = struct.pack('!BH100s', accion, idArch, referencia.encode('ascii'))
+else:
+	paquete = struct.pack('!BH', accion, idArch)
+	
 sock.sendto(paquete, address)
 
+#print("hola1")
 payload, client_address = sock.recvfrom(1035) 
+#print("hola2")
+paqDep = struct.unpack('!100s', payload[0:])
+respuesta = paqDep[0].decode()
 
 #Decirle al usario el resultado de la consulta
-
-
+print(respuesta)
 print("Accion del cliente liviano termino")
--- NodoVerde.py
+++ NodoVerde.py
@@ -3,7 +3,7 @@ import threading
 import struct
 from USL import USL
 from netifaces import interfaces, ifaddresses, AF_INET
-from USL import USL
+from a_aPaq import a_aPaq
 
 try:
     import queue
@@ -12,81 +12,156 @@ except ImportError:
 
 class NodoVerde:
 
-    # Aqui se ponen los detalles para ajusta puerto y IP
-    def __init__(self, port, ip_azul, puerto_azul):
-        self.ip = ifaddresses(interfaces()[1])[AF_INET].pop(0)['addr']
-        self.port = port
-        self.ip_azul = ip_azul
-        self.puerto_azul = puerto_azul
-        self.colaEntrada = queue.Queue()
-        self.colaSalida = queue.Queue()
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		self.secure_udp = USL(self.ip, self.puerto, 5)
+	# Aqui se ponen los detalles para ajusta puerto y IP
+	def __init__(self, port, ip_azul, puerto_azul, my_id):
+		self.ip = ifaddresses(interfaces()[1])[AF_INET].pop(0)['addr']
+		self.port = port
+		self.ip_azul = ip_azul
+		self.puerto_azul = puerto_azul
+		self.my_id = my_id   #ID de este nodo verde (193-223)
+		self.colaEntrada = queue.Queue()
+		self.colaSalida = queue.Queue()
+		self.colaSalidaCliente = queue.Queue()
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.secure_udp = USL(self.ip, self.port, 5)
 
-    def run(self):
-        server = (self.ip, self.port)
-        self.sock.bind(server)
-        print("Escuchando: " + self.ip + ":" + str(self.port))
+	def run(self):
+		server = (self.ip, self.port)
+		self.sock.bind(server)
+		print("Escuchando: " + self.ip + ":" + str(self.port))
+		
+		#Por si queremos consultar un archivo que no sea de esta maquina
+		#self.my_id = int(input("Digite otro ID de verde"))
 
-        ##Hilos recibidor
-        t = threading.Thread(target=self.HiloRecibidor)
-        t.start()
-        print("hilo recibidor iniciado")
-        ##Hilo enviador
-        t2 = threading.Thread(target=self.HiloEnviador)
-        t2.start()
-        print(("hilo enviador iniciado"))
-        ##Hilo logico
-        t3 = threading.Thread(target=self.HiloLogico)
-        t3.start()
-        print("hilo logico iniciado")
-        ##Hilo recibidor de cliente
-        t4 = threading.Thread(target=self.RecibirCliente)
-        t4.start()
-        print("hilo recibir de cliente iniciado")
+		##Hilos recibidor
+		t = threading.Thread(target=self.HiloRecibidor)
+		t.start()
+		print("hilo recibidor iniciado")
+		##Hilo enviador
+		t2 = threading.Thread(target=self.HiloEnviador)
+		t2.start()
+		print(("hilo enviador iniciado"))
+		##Hilo recibidor de cliente
+		t3 = threading.Thread(target=self.RecibirCliente)
+		t3.start()
+		print("hilo recibir de cliente iniciado")
 
 	def RecibirCliente(self):
-        while True:
-            payload, client_address = self.sock.recvfrom(1035)  # recibe 1035 bytes y la (direcciónIP, puerto) del origen
-            tipo = int.from_bytes(payload[0:1], byteorder=('little'))
-            if tipo == 0:
-                # caso Depositar
-                
-            if tipo == 1:
-                # caso Existe
-                
-            sock.sendto(paquete, client_address)
+		while True:
+			payload, client_address = self.sock.recvfrom(1035)  # recibe 1035 bytes y la (direcciónIP, puerto) del origen
+			paquete = struct.unpack('!BH', payload[0:3])
+			tipo = paquete[0]
+			idArch = paquete[1]
+			if (tipo == 1):
+				# caso Depositar
+				paqDep = struct.unpack('!100s', payload[3:])
+				referencia = paqDep[0].decode()
+				arrayChunks = self.particionar(referencia)
+				self.union(arrayChunks)
+				tamArray = len(arrayChunks)
+				
+				for x in range(tamArray):
+					paqEnv = a_aPaq(0, self.my_id, idArch, x, arrayChunks[x])
+					bytesP = paqEnv.serialize()
+					self.colaSalida.put(bytesP)
+				
+			if (tipo == 2):
+				# caso Existe
+				paqEnv = a_aPaq(2, self.my_id, idArch, 0, 0)
+				bytesP = paqEnv.serialize()
+				self.colaSalida.put(bytesP)
+				
+				
+			if (tipo == 3):
+				# caso Completo
+				paqEnv = a_aPaq(4, self.my_id, idArch, 0, 0)
+				bytesP = paqEnv.serialize()
+				self.colaSalida.put(bytesP)
+				
+			if (tipo == 4):
+				# caso Obtener
+				paqEnv = a_aPaq(6, self.my_id, idArch, 0, 0)
+				bytesP = paqEnv.serialize()
+				self.colaSalida.put(bytesP)
+				
+			if (tipo == 5):
+				# caso Localizar
+				paqEnv = a_aPaq(8, self.my_id, idArch, 0, 0)
+				bytesP = paqEnv.serialize()
+				self.colaSalida.put(bytesP)
+				
+			if (tipo == 6):
+				# caso Eliminar
+				paqEnv = a_aPaq(10, self.my_id, idArch, 0, 0)
+				bytesP = paqEnv.serialize()
+				self.colaSalida.put(bytesP)
+				 
+			respuesta = self.colaSalidaCliente.get()
+			paqResp = struct.pack('!100s', respuesta.encode('ascii'))
+			
+			#print(client_address)
+			sock.sendto(paqResp, client_address)
 		
-    def HiloRecibidor(self):
-        while True:
+	def HiloRecibidor(self):
+		while True:
 			payload , address = self.secure_udp.recibir()
-            tipo = int.from_bytes(payload[0:1], byteorder=('little'))
-            if tipo == 0:
-                # caso PUT
-                
-            if tipo == 2:
-                # caso EXISTS
+			tipo = int.from_bytes(payload[0:1], byteorder=('big'))
+			if (tipo == 3): #problema porque todos los uslPaq tienen 0 o 1 en la primera posicion 
+				# caso respuesta a EXISTS
+				resp = "El archivo que consulto si existe"
+				colaSalidaCliente.put(resp)
+				
+			if (tipo == 5):
+				# caso respuesta a COMPLETE
+				resp = "El archivo que consulto si etsa completo"
+				colaSalidaCliente.put(resp)
+				
+			if (tipo == 7):
+				# caso respuesta a GET
+				nombre = "temporal"
+				resp = "El archivo que consulto se guardo como " + nombre
+				colaSalidaCliente.put(resp)
+				
+			if (tipo == 9):
+				# caso respuesta a LOCATE
+				nombreNodo = "Nodo temporal"
+				resp = "El archivo que consulto si existe en nodo " + nombreNodo
+				colaSalidaCliente.put(resp)
 
-    def HiloEnviador(self):
-        while True:
-            #Saca paquete de la cola; espera si cola esta vacia
-            bytePacket = self.colaSalida.get()
-			address = (self.ip_azul, self.puerto_azul)
+	def HiloEnviador(self):
+		while True:
+			#Saca paquete de la cola; espera si cola esta vacia
+			bytePacket = self.colaSalida.get()
 			try:
 				#manda al nodo azul
-				self.sock.sendto(bytePacket, address)
+				self.secure_udp.send(bytePacket, self.ip_azul, self.puerto_azul)
 			except OSError as err:
 				print("Failed on addressing: ", err)
-
-    def HiloLogico(self):
 		
-		
-	def particionar(self):
+	def particionar(self, referencia):
 		#metodo para particionar archivos en chunks
-		#podria devolver un arreglo de chunks de 1KB
+		#Devuelve un arreglo de chunks\
+		print(referencia)
+		#with open(referencia, "rb") as binary_file:
+		with open("imagenes/hola.jpg", "rb") as binary_file:
+			data = binary_file.read()
 		
-	def union(self):
+		tam = len(data)
+		tamArray = int(tam/1000)+1
+		arrayChunks = [None] * tamArray
+		i = 0
+		
+		for x in range(tamArray-1):
+			arrayChunks[x] = data[i:i+1000]
+			i += 1000;
+		
+		arrayChunks[tamArray-1] = data[i:tam]
+		
+		return arrayChunks	
+		
+	def union(self, arrayChunks):
 		#metodo para reconstruir un archivo
 		
-	def seleccionNodoAzul(self):
+	def seleccionNodoAzul(self): #por ahora esta hardcoded
 		#selecciona con cual nodo azul se va a comunicar
+		variableTemporal = 787878;
